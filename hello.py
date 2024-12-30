50a1cf30ddf541bd3b25b8544a6d9fecc5e36cee
Your Name <you@example.com>
2024-12-30 18:49:42 +0530

    msg

---
+++ hello.py
@@ -0,0 +1,32 @@
+print("Hello world")
+
+# #variable
+
+# #int
+# result = 10+20
+# print (result)
+
+# #print(10+20)
+# print("Memory address: ", result)
+
+# #there are metonds specific to that data type
+# print("Data type: ", type(result))
+
+# a= 19.02
+# b= 21.10
+# res=a+b
+# print("result:", res/"Type:", type(res))
+
+# txt = "Hello and welcome to my world!"
+# x = txt.casefold()
+# print(x)
+
+# print(txt.split(" "))
+# print(txt.find("H"))
+# print(txt.swapcase())
+
+# sample_str = "hello"
+# print(sample_str.index("h"))
+# sample_chart = sample_str[-1]
+# print(sample_chart)
+# print(len(sample_chart))
