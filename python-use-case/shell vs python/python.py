457e319ea1ee10a881cc759cdc2f30726a93371e
Your Name <you@example.com>
2025-05-08 09:22:22 +0530

    msg

---
+++ python-use-case/shell vs python/python.py
@@ -0,0 +1,31 @@
+The choice between Shell Scripting and Python in DevOps depends on what you're trying to do. Both are useful but work best in different situations:
+
+✅ Use Shell Scripting when:
+You’re doing basic system tasks like starting services, creating files, or managing users.
+
+You just need to run command-line tools or Linux commands.
+
+You want a quick and simple solution for one-time tasks.
+
+You're working with logs or text files and need to search, cut, or replace text.
+
+You need to set or manage environment variables or simple configs.
+
+✅ Use Python when:
+Your task is more complex and needs logic, loops, or data structures.
+
+You want the script to work the same on Windows, Linux, and macOS.
+
+You're working with APIs, web services, or databases.
+
+You plan to reuse or expand the script into something bigger later.
+
+You want better error handling and easier debugging.
+
+You’re processing data or doing analytics or automation with tools like Pandas or NumPy.
+
+In short:
+
+Shell is best for quick system tasks.
+
+Python is best for complex, scalable, or cross-platform tasks.
